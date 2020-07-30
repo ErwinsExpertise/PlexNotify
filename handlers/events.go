7782f43b11b6d292fbad0ed21bbf933a59f8aa3e
ErwinsExpertise <evanserwin@gmail.com>
2020-07-30 07:27:00 -0500

    Updated metric timing

--- handlers/events.go
+++ handlers/events.go
@@ -94,7 +94,7 @@ func EventHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	metrics.EventCollector(time.Since(start).Milliseconds())
+	metrics.EventCollector(int64(time.Since(start) / 1000000))
 
 	log.Printf("Processing event type: %s | User: %s | Title: %s | IP: %s", resp, eventLoad.Account.Title, eventLoad.CheckTitle(), eventLoad.Player.PublicAddress)
 	AppendActivity(time.Now().Format("2006-01-02 15:04:05"), resp, eventLoad.Account.Title, eventLoad.CheckTitle(), eventLoad.Player.PublicAddress)
